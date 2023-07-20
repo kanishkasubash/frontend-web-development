f60a4ccd0c5a4f01d5af9811f437653a5f08176a
kanishkasubash <kanishkasubash@gmail.com>
2023-07-21 01:10:23 +0530

    modified:   javascript-crash-course/js/functions.js

--- javascript-crash-course/js/functions.js
+++ javascript-crash-course/js/functions.js
@@ -3,20 +3,24 @@ function greeting() {
     console.log("Hello World!");
 }
 
-function add(num1, num2) {
-    return num1 + num2;
-}
-
 function areaOfCircle(radius) {
     const PI = 3.14;
     area = PI * (radius * radius);
     return area;
 }
 
+// Define Arrow Functions
+sum = (num1, num2) => {return num1 + num2;}
+sub = (num1, num2) => {return num1 - num2;}
+mul = (num1, num2) => {return num1 * num2;}
+div = (num1, num2) => {return num1 / num2;}
+
 // Call Functions
 greeting();
 
-let sum = add(10, 20);
-console.log("SUM = " + sum);
-
 console.log(`Area of the Circle = ${areaOfCircle(7)}`);
+
+console.log(`SUM = ${sum(5, 3)}`);
+console.log(`SUB = ${sub(5, 3)}`);
+console.log(`MUL = ${mul(5, 3)}`);
+console.log(`DIV = ${div(5, 3)}`);
