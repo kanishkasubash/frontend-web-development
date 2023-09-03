8eaacf54862fbe81bc21fa7cf92e24639b078232
kanishkasubash <kanishkasubash@gmail.com>
2023-09-03 20:52:32 +0530

    modified:   typescript-demo/datatypes.js 	modified:   typescript-demo/datatypes.ts

--- typescript-demo/datatypes.js
+++ typescript-demo/datatypes.js
@@ -1,5 +1,22 @@
 "use strict";
+/* string data type */
 let fname = 'kanishka';
 let lname = 'Priyashantha';
-let fullname = `${fname} ${lname}`;
-console.log(fullname.toUpperCase());
+let fullname = `${fname} ${lname}`.toUpperCase();
+console.log(fullname);
+/* number data type */
+let age;
+age = 25;
+let dob = "25";
+let date = parseInt(dob);
+console.log(date);
+/* boolean data type */
+let isValid = false;
+console.log(isValid);
+/* array data type */
+let empList;
+empList = ["Emp1", "Emp2", "Emp3", "Emp4"];
+let numList;
+numList = [1, 2, 3, 4, 5];
+console.log(empList);
+console.log(numList);
--- typescript-demo/datatypes.ts
+++ typescript-demo/datatypes.ts
@@ -1,6 +1,30 @@
-let fname : string = 'kanishka';
-let lname : string = 'Priyashantha';
+/* string data type */
+let fname: string = 'kanishka';
+let lname: string = 'Priyashantha';
 
-let fullname = `${fname} ${lname}`
+let fullname = `${fname} ${lname}`.toUpperCase();
 
-console.log(fullname.toUpperCase());
+console.log(fullname);
+
+/* number data type */
+let age: number;
+age = 25;
+
+let dob: string = "25";
+let date: number = parseInt(dob);
+
+console.log(date);
+
+/* boolean data type */
+let isValid: boolean = false;
+
+console.log(isValid);
+
+/* array data type */
+let empList: string[];
+empList = ["Emp1", "Emp2", "Emp3", "Emp4"]
+let numList: Array<number>;
+numList = [1, 2, 3, 4, 5];
+
+console.log(empList);
+console.log(numList);
