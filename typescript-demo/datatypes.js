fe2ff65d0e132d8956c14c8b52cf62d5950cb812
kanishkasubash <kanishkasubash@gmail.com>
2023-09-03 22:18:22 +0530

    modified:   typescript-demo/datatypes.js 	modified:   typescript-demo/datatypes.ts

--- typescript-demo/datatypes.js
+++ typescript-demo/datatypes.js
@@ -17,6 +17,21 @@ console.log(isValid);
 let empList;
 empList = ["Emp1", "Emp2", "Emp3", "Emp4"];
 let numList;
-numList = [1, 2, 3, 4, 5];
+numList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(empList);
 console.log(numList);
+let squar = numList.map((num) => num * num);
+console.log(squar);
+let rep = numList.map((num, index) => num.toString().repeat(index + 1)).join(", ");
+console.log(rep);
+let sum = numList.reduce((acc, num) => acc + num);
+console.log(sum);
+/* enum data type */
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let bg = Color.Green;
+console.log(bg);
