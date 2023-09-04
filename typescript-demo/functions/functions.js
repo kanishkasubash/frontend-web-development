d126eb8130cc74153de9b6e35301b59e1e46c6fe
kanishkasubash <kanishkasubash@gmail.com>
2023-09-05 01:51:54 +0530

    modified:   typescript-demo/functions/functions.js 	modified:   typescript-demo/functions/functions.ts

--- typescript-demo/functions/functions.js
+++ typescript-demo/functions/functions.js
@@ -1,11 +1,8 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sumArray2 = exports.sumArray = exports.findNeedle = void 0;
 /* find elemrnt using indexOf method */
 function findNeedle(haystack) {
     return `found the needle at position ${haystack.indexOf('needle')}`;
 }
-exports.findNeedle = findNeedle;
 console.log(findNeedle(['3', '123124234', null, 'needle', 'world', 'hay', 2, '3', true, false]));
 /**
  * Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
@@ -16,7 +13,6 @@ console.log(findNeedle(['3', '123124234', null, 'needle', 'world', 'hay', 2, '3'
 function sumArray(array) {
     return !array || array.length <= 2 ? 0 : array.sort((a, b) => a - b).slice(1, -1).reduce((p, n) => p + n, 0);
 }
-exports.sumArray = sumArray;
 /**
  * Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
  * @param array
@@ -25,14 +21,13 @@ exports.sumArray = sumArray;
 function sumArray2(array) {
     return !array || array.length <= 2 ? 0 : (array.reduce((a, b) => a + b, 0) - Math.max(...array) - Math.min(...array));
 }
-exports.sumArray2 = sumArray2;
 console.log(sumArray([6, 2, 5, 2, 6]));
 console.log(sumArray2([6, 2, 5, 2, 6]));
 /**
  * Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
  * @param fatherAge Current father's age (years)
  * @param sonAge Current age of his son (years)
- * @returns Age difference (years)
+ * @returns Age twice as old (years)
  */
 function yearsAgoOrInFuture(fatherAge, sonAge) {
     return Math.abs(fatherAge - (2 * sonAge));
--- typescript-demo/functions/functions.ts
+++ typescript-demo/functions/functions.ts
@@ -1,5 +1,5 @@
 /* find elemrnt using indexOf method */
-export function findNeedle(haystack: any[]): string {
+function findNeedle(haystack: any[]): string {
     return `found the needle at position ${haystack.indexOf('needle')}`;
 }
 
@@ -11,7 +11,7 @@ console.log(findNeedle(['3', '123124234', null, 'needle', 'world', 'hay', 2, '3'
  * @param array 
  * @returns 
  */
-export function sumArray(array: number[] | null): number {
+function sumArray(array: number[] | null): number {
 
     return !array || array.length <= 2 ? 0 : array.sort((a, b) => a - b).slice(1, -1).reduce((p, n) => p + n, 0);
 }
@@ -21,7 +21,7 @@ export function sumArray(array: number[] | null): number {
  * @param array 
  * @returns 
  */
-export function sumArray2(array: number[] | null): number {
+function sumArray2(array: number[] | null): number {
 
     return !array || array.length <= 2 ? 0 : (array.reduce((a, b) => a + b, 0) - Math.max(...array) - Math.min(...array));
 }
